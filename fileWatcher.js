7b166f06ceb8fc4e279701f29db6361cf58a3b37
Tim <timdobranski@gmail.com>
2023-12-13 15:16:25 -0800

    File watcher now gets GP metadata, then gets Spotify metadata. Uploads and file entries do not yet contain the necessary data yet

--- fileWatcher.js
+++ fileWatcher.js
@@ -1,14 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 const supabase = require('./supabase');
+const alphaTab = require('@coderline/alphatab');
+const findSongOnSpotify = require('./spotify');
 
-// Replace with the path to your guitarProFiles folder
+// Update this to instead just send a request to the addSong, removeSong, or updateSong endpoint in the main app
 const folderPath = path.join('/Users', 'timdobranski', 'Desktop', 'guitarProFiles');
+
+async function getScoreMetadata(guitarProFilePath) {
+  try {
+    // Read the Guitar Pro file into a buffer
+    const fileData = await fs.promises.readFile(guitarProFilePath);
+
+    // Create a Uint8Array from the buffer
+    const uint8Array = new Uint8Array(fileData);
+
+    // Assuming alphaTab has a similar method in Node.js
+    const settings = new alphaTab.Settings();
+    const score = alphaTab.importer.ScoreLoader.loadScoreFromBytes(uint8Array, settings);
+
+    // Extract metadata from the score
+    const metadata = {
+      title: score.title,
+      subtitle: score.subTitle,
+      artist: score.artist,
+      album: score.album,
+      words: score.words,
+      music: score.music,
+      // copyright: score.copyright,
+      // tab: score.tab,
+      instructions: score.instructions,
+      tempo: score.tempoLabel,
+      key: score.masterBars[0].keySignature
+    };
+
+
+    return metadata;
+  } catch (error) {
+    console.error('Error loading Guitar Pro file:', error);
+    throw error;
+  }
+}
+
 
 fs.watch(folderPath, async (eventType, filename) => {
   if (filename) {
     console.log(`${filename} has been ${eventType}`);
-
     const filePath = path.join(folderPath, filename);
     const fileExists = fs.existsSync(filePath);
 
@@ -18,7 +55,15 @@ fs.watch(folderPath, async (eventType, filename) => {
         console.log(`Uploading ${filename} to Supabase storage and adding to songs table.`);
 
         try {
-          // Upload file to Supabase storage
+          // STEP 1/4: get gp metadata
+          const metadata = await getScoreMetadata(filePath);
+          console.log('guitar pro metadata:', metadata);
+
+          // STEP 2/4: get spotify metadata
+          const spotifyMetadata = await findSongOnSpotify(metadata.title, metadata.artist);
+          console.log('spotify metadata:', spotifyMetadata);
+
+          // STEP 3/4: Upload file to Supabase storage
           const fileContent = fs.readFileSync(filePath);
           const { data: storageResponse, error: storageError } = await supabase.storage
             .from('teacher-storage')
@@ -26,12 +71,10 @@ fs.watch(folderPath, async (eventType, filename) => {
 
           if (storageError) {
             console.log('ERROR IN STORAGE UPLOAD:');
-            throw storageError; // Use storageError directly
+            throw storageError;
           }
-
           console.log(`File ${filename} uploaded to Supabase storage.`);
-
-          // Add entry to songs table
+          // STEP 4/4: Add entry to songs table
           const { data, error } = await supabase
             .from('songs')
             .insert([{
@@ -39,7 +82,7 @@ fs.watch(folderPath, async (eventType, filename) => {
               gp_url: `songs/${filename}`
             }]);
 
-          if (error) { // Use error directly
+          if (error) {
             throw error;
           }
           console.log(`Entry for ${filename} added to songs table.`);
---
+++ spotify.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+const querystring = require('querystring');
+require('dotenv').config();
+
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+console.log('clientId:', clientId);
+console.log('clientSecret:', clientSecret);
+
+async function getSpotifyAccessToken(clientId, clientSecret) {
+    const headers = {
+        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    const data = querystring.stringify({ grant_type: 'client_credentials' });
+
+    try {
+        const response = await axios.post('https://accounts.spotify.com/api/token', data, { headers });
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Error getting Spotify access token:', error);
+        throw error;
+    }
+}
+
+async function searchSpotifySong(songName, artistName, accessToken) {
+  const query = encodeURIComponent(`${songName} artist:${artistName}`);
+  const url = `https://api.spotify.com/v1/search?q=${query}&type=track&limit=1`;
+
+  const headers = {
+      'Authorization': `Bearer ${accessToken}`
+  };
+
+  try {
+      const response = await axios.get(url, { headers });
+      return response.data.tracks.items[0]; // Returns the first matching track
+  } catch (error) {
+      console.error('Error searching Spotify song:', error);
+      throw error;
+  }
+}
+
+async function findSongOnSpotify(songName, artistName) {
+
+  try {
+      const accessToken = await getSpotifyAccessToken(clientId, clientSecret);
+      const song = await searchSpotifySong(songName, artistName, accessToken);
+
+      if (song) {
+          console.log('Song found:', song);
+          // Process the song information as needed
+      } else {
+          console.log('Song not found');
+      }
+  } catch (error) {
+      console.error('Error finding song on Spotify:', error);
+  }
+}
+
+
+
+module.exports = findSongOnSpotify;
